ad2f8874e50a9885e0b5cf87743a1cdca0206d58
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2018-05-23 11:49:42 -0500

    Optimizando

--- FraccMethod/Interface/IMethod.cs
+++ FraccMethod/Interface/IMethod.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FraccEntities;
+﻿using FraccEntities;
+using System;
 
 namespace FraccMethod
 {
