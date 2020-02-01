c6a199f283269116d6f5da2d2e8ccb58842df5c2
Winston Rodriguez <wijurost@gmail.com>
2020-02-01 11:02:37 -0500

    Agregando test

---
+++ FraccTest/FraccMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FraccMethod;
+using FraccEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FraccTest
+{
+    [TestClass]
+    public class FraccMethod
+    {
+        private readonly Method _method;
+
+        public FraccMethod()
+        {
+            _method = new Method();
+        }
+
+        [TestMethod]
+        public void EsHomogenea()
+        {
+            Fraccion f1 = new Fraccion { Numerador = 1, Denominador = 2 };
+            Fraccion f2 = new Fraccion { Numerador = 1, Denominador = 2 };
+            var result = _method.EsHomogenea(f1,f2);
+            Assert.IsTrue(result, "Si tienen los mismo denominadores son homogeneas");
+        }
+
+        [TestMethod]
+        public void esHeterogenea()
+        {
+            Fraccion f1 = new Fraccion { Numerador = 1, Denominador = 2 };
+            Fraccion f2 = new Fraccion { Numerador = 1, Denominador = 3 };
+            var result = _method.EsHomogenea(f1, f2);
+            Assert.IsFalse(result, "Si no son iguales no es homogenea por lo tanto es heterogenea");
+        }
+    }
+}
---
+++ FraccTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FraccTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FraccTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("96d348ab-1deb-4805-b335-f7184ec5d671")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
